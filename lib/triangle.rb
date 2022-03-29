f0f825609ee5bbe6eee7a9462f10096ebfdf36a2
Fred <higgins.fred.s@gmail.com>
2022-03-29 10:05:01 -0700

    commit tree 355481c8c47901264028904a605e63785652a231

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :equilateral, :isosceles, :scalene, :left, :right, :bottom
+
+  
+
+  def initialize(l1, l2, l3)
+
+    @left = l1
+    @right = l2
+    @bottom = l3
+    
+  end
+
+  def kind
+    if @left + @right + @bottom != 0 && @left.positive? && @right.positive? && @bottom.positive? && @left + @right > @bottom && @left + @bottom > @right && @right + @bottom > @left
+      @triangle = []
+      combine
+  
+      sum = @triangle.uniq.size
+  
+      if sum == 1
+        :equilateral
+      elsif sum == 2
+        :isosceles
+      else
+        :scalene
+      end
+  
+    else
+        raise TriangleError
+    end
+  end
+
+  private
+
+  def combine
+    @triangle << left
+    @triangle << right
+    @triangle << bottom
+  end
+
+
+  class TriangleError < StandardError
+    def message
+      "No length detected"
+    end
+  end
+
+
 end
